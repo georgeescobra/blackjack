c72458e15082ec49489ee790b7470d69b15f1171
George <tgeorge.escobar@gmail.com>
2020-12-16 20:29:52 -0800

    javascript skeleton

---
+++ javasscript/main.js
@@ -0,0 +1,10 @@
+function startGame(titleName) {
+    //welcomeScreen();
+    document.getElementById(titleName).innerHTML = "WELCOME TO TSING's BLACKJACK TABLE";
+}
+
+
+
+function renderImages() {
+
+}
